77ae2b3ebab2e84d18b8b891c01a8d8b39005d32
Jessy Provenzal <jessyprovenzal@hotmail.com>
2023-11-15 15:24:59 +0100

    Added trails and soudns for car

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -16,18 +16,22 @@ public class CarController : MonoBehaviour
     public Transform Left;
     public Transform Straight;
 
-    // public TrailRenderer Trail1;
-    // public TrailRenderer Trail2;
-    // public TrailRenderer Trail3;
-    // public TrailRenderer Trail4;
+    public TrailRenderer Trail1;
+    public TrailRenderer Trail2;
+    public TrailRenderer Trail3;
+    public TrailRenderer Trail4;
 
-    // public AudioSource CarEngine;
-    // public AudioSource CarDrift;
-    // public bool DriftCheck;
+    public AudioSource CarEngine;
+    public AudioSource CarDrift;
+    public bool DriftCheck;
+    private bool hasStartedMoving = false;
+
 
     private void Start()
     {
         RB = gameObject.GetComponent<Rigidbody>();
+            RB.useGravity = true; // Add this line to enable gravity
+
     }
 
     private void Update()
@@ -41,15 +45,21 @@ public class CarController : MonoBehaviour
             Wheel3.Rotate(500 , 0, 0  * Time.deltaTime);
             Wheel4.Rotate(500 , 0, 0  * Time.deltaTime);
 
-            // if(CarEngine.isPlaying == false)
-            // {
-            //     CarEngine.Play();
-            // }
+            if (!hasStartedMoving)
+            {
+                print("Car has started moving");
+                CarEngine.Play();
+                hasStartedMoving = true;
+            }
         }
-        // else
-        // {
-        //     CarEngine.Stop();
-        // }
+        else
+        {
+            CarEngine.Stop();
+        }
+        if (!Input.GetKey("w"))
+        {
+            hasStartedMoving = false; // Reset the flag when the car stops moving
+        }
 
         if (Input.GetKey("s"))
         {
@@ -67,6 +77,11 @@ public class CarController : MonoBehaviour
             CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Left.rotation, 4 * Time.deltaTime);
             RB.velocity += CarBody.transform.forward * 120 * Time.deltaTime;
             RB.velocity -= transform.forward * 110 * Time.deltaTime;
+            // Limit upward velocity
+            if (RB.velocity.y > 0)
+            {
+                RB.velocity = new Vector3(RB.velocity.x, 0, RB.velocity.z);
+            }
         }
 
         if(Input.GetKey("d") && Input.GetKey("w"))
@@ -75,6 +90,11 @@ public class CarController : MonoBehaviour
             CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Right.rotation, 4 * Time.deltaTime);
             RB.velocity += CarBody.transform.forward * 120 * Time.deltaTime;
             RB.velocity -= transform.forward * 110 * Time.deltaTime;
+            // Limit upward velocity
+            if (RB.velocity.y > 0)
+            {
+                RB.velocity = new Vector3(RB.velocity.x, 0, RB.velocity.z);
+            }
         }
 
         if(!Input.GetKey("d") && !Input.GetKey("a"))
@@ -82,27 +102,27 @@ public class CarController : MonoBehaviour
             CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Straight.rotation, 4 * Time.deltaTime);
         }
 
-        // if(CarBody.transform.localRotation.y * 100 > 15 || CarBody.transform.localRotation.y * 100 < -15)
-        // {
-        //     Trail1.emitting = true;
-        //     Trail2.emitting = true;
-        //     Trail3.emitting = true;
-        //     Trail4.emitting = true;
+        if(CarBody.transform.localRotation.y * 100 > 15 || CarBody.transform.localRotation.y * 100 < -15)
+        {
+            Trail1.emitting = true;
+            Trail2.emitting = true;
+            Trail3.emitting = true;
+            Trail4.emitting = true;
 
-        //     if(DriftCheck == false)
-        //     {
-        //         DriftCheck = true;
-        //         CarDrift.Play();
-        //     }
-        // }
-        // else
-        // {
-        //     DriftCheck = false;
+            if(DriftCheck == false)
+            {
+                DriftCheck = true;
+                CarDrift.Play();
+            }
+        }
+        else
+        {
+            DriftCheck = false;
 
-        //     Trail1.emitting = false;
-        //     Trail2.emitting = false;
-        //     Trail3.emitting = false;
-        //     Trail4.emitting = false;
-        // }
+            Trail1.emitting = false;
+            Trail2.emitting = false;
+            Trail3.emitting = false;
+            Trail4.emitting = false;
+        }
     }
 }
