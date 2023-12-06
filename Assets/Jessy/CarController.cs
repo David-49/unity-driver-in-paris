831d196968b5980105700c57ce68e5905476e854
Jessy Provenzal <jessyprovenzal@hotmail.com>
2023-12-06 10:07:10 +0100

    NewCar Jessy

--- Assets/Jessy/CarController.cs
+++ Assets/Jessy/CarController.cs
@@ -2,137 +2,122 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarController : MonoBehaviour
+public class carController : MonoBehaviour
 {
-    private Rigidbody RB;
-    public GameObject CarBody;
+    // Définition des constantes pour les axes de contrôle
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
 
-    public Transform Wheel1;
-    public Transform Wheel2;
-    public Transform Wheel3;
-    public Transform Wheel4;
+    // Variables pour les entrées utilisateur
+    private float horizontalInput;
+    private float verticalInput;
+    private float currentSteerAngle;
+    private float currentbreakForce;
+    private bool isBreaking;
 
-    public Transform Right;
-    public Transform Left;
-    public Transform Straight;
+    // Paramètres modifiables dans l'éditeur Unity
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteerAngle;
 
-    public TrailRenderer Trail1;
-    public TrailRenderer Trail2;
-    public TrailRenderer Trail3;
-    public TrailRenderer Trail4;
-    public float speedForward = 150;
-    public float speedReverse = 80;
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider;
+    [SerializeField] private WheelCollider rearRightWheelCollider;
 
+    [SerializeField] private Transform frontLeftWheelTransform;
+    [SerializeField] private Transform frontRightWheeTransform;
+    [SerializeField] private Transform rearLeftWheelTransform;
+    [SerializeField] private Transform rearRightWheelTransform;
+    // [SerializeField] private AudioSource CarEngine;
+    // [SerializeField] private AudioSource CarDrift;
 
-    public AudioSource CarEngine;
-    public AudioSource CarDrift;
-    public bool DriftCheck;
-    private bool hasStartedMoving = false;
-
-
-    private void Start()
+    // Méthode appelée à chaque frame fixe
+    private void FixedUpdate()
     {
-        RB = gameObject.GetComponent<Rigidbody>();
-            RB.useGravity = true; // Add this line to enable gravity
+        // Obtenir les entrées utilisateur
+        GetInput();
 
+        // Gérer la motorisation
+        HandleMotor();
+
+        // Gérer la direction
+        HandleSteering();
+
+        // Mettre à jour la rotation des roues
+        UpdateWheels();
     }
 
-    private void Update()
+    // Méthode pour obtenir les entrées utilisateur
+    private void GetInput()
     {
-        if (Input.GetKey("w"))
-        {
-            RB.velocity -= transform.forward * speedForward * Time.deltaTime;
+        horizontalInput = Input.GetAxis(HORIZONTAL);
+        verticalInput = Input.GetAxis(VERTICAL);
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
 
-            Wheel1.Rotate(-500 , 0, 0 * Time.deltaTime);
-            Wheel2.Rotate(-500 , 0, 0 * Time.deltaTime);
-            Wheel3.Rotate(500 , 0, 0  * Time.deltaTime);
-            Wheel4.Rotate(500 , 0, 0  * Time.deltaTime);
+    // Méthode pour gérer la motorisation
+    private void HandleMotor()
+    {
+        // Appliquer la force motrice aux roues avant
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
 
-            if (!hasStartedMoving)
-            {
-                print("Car has started moving");
-                CarEngine.Play();
-                hasStartedMoving = true;
-            }
-        }
-        else
-        {
-            CarEngine.Stop();
-        }
-        if (!Input.GetKey("w"))
-        {
-            hasStartedMoving = false; // Reset the flag when the car stops moving
-        }
+        // Démarrer le son du moteur (ne fonctionne pas)
+        // CarEngine.play();
 
-        if (Input.GetKey("s"))
-        {
-            RB.velocity += transform.forward * speedReverse * Time.deltaTime;
+        // Définir la force de freinage actuelle en fonction de la décélération
+        currentbreakForce = isBreaking ? breakForce : 0f;
 
-            Wheel1.Rotate(500 ,0, 0 * Time.deltaTime);
-            Wheel2.Rotate(500 ,0, 0 * Time.deltaTime);
-            Wheel3.Rotate(-500 ,0, 0 * Time.deltaTime);
-            Wheel4.Rotate(-500 ,0, 0 * Time.deltaTime);
-        }
+        // Appliquer la force de freinage
+        ApplyBreaking();
+    }
 
-        if(Input.GetKey("a") && Input.GetKey("w"))
-        {
-            transform.Rotate(0, -30 * Time.deltaTime, 0);
-            
-            CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Left.rotation, 4 * Time.deltaTime);
-            Wheel1.transform.rotation = Quaternion.Lerp(Wheel1.transform.rotation, Right.rotation, 4 * Time.deltaTime);
-            Wheel3.transform.rotation = Quaternion.Lerp(Wheel3.transform.rotation, Right.rotation, 4 * Time.deltaTime);
-            RB.velocity += CarBody.transform.forward * 120 * Time.deltaTime;
-            RB.velocity -= transform.forward * 110 * Time.deltaTime;
-            // Limit upward velocity
-            if (RB.velocity.y > 0)
-            {
-                RB.velocity = new Vector3(RB.velocity.x, 0, RB.velocity.z);
-            }
-        }
+    // Méthode pour appliquer la force de freinage
+    private void ApplyBreaking()
+    {
+        // Appliquer la force de freinage à toutes les roues
+        frontRightWheelCollider.brakeTorque = currentbreakForce;
+        frontLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearRightWheelCollider.brakeTorque = currentbreakForce;
 
-        if(Input.GetKey("d") && Input.GetKey("w"))
-        {
-            transform.Rotate(0, 30 * Time.deltaTime, 0);
-            CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Right.rotation, 4 * Time.deltaTime);
-            Wheel1.transform.rotation = Quaternion.Lerp(Wheel1.transform.rotation, Right.rotation, 4 * Time.deltaTime);
-            Wheel3.transform.rotation = Quaternion.Lerp(Wheel3.transform.rotation, Right.rotation, 4 * Time.deltaTime);
-            RB.velocity += CarBody.transform.forward * 120 * Time.deltaTime;
-            RB.velocity -= transform.forward * 110 * Time.deltaTime;
-            // Limit upward velocity
-            if (RB.velocity.y > 0)
-            {
-                RB.velocity = new Vector3(RB.velocity.x, 0, RB.velocity.z);
-            }
-        }
+        // Jouer le son de dérapage
+        // Ne fonctionne pas
+        // CarDrift.Play(); // Jouer le son de dérapage
+    }
 
-        if(!Input.GetKey("d") && !Input.GetKey("a"))
-        {
-            CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Straight.rotation, 4 * Time.deltaTime);
-            Wheel1.transform.rotation = Quaternion.Lerp(Wheel1.transform.rotation, Straight.rotation, 4 * Time.deltaTime);
-            Wheel3.transform.rotation = Quaternion.Lerp(Wheel3.transform.rotation, Straight.rotation, 4 * Time.deltaTime);
-        }
+    // Méthode pour gérer la direction
+    private void HandleSteering()
+    {
+        // Calculer l'angle de braquage actuel
+        currentSteerAngle = maxSteerAngle * horizontalInput;
 
-        if(CarBody.transform.localRotation.y * 100 > 15 || CarBody.transform.localRotation.y * 100 < -15)
-        {
-            Trail1.emitting = true;
-            Trail2.emitting = true;
-            Trail3.emitting = true;
-            Trail4.emitting = true;
+        // Appliquer l'angle de braquage aux roues avant
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
 
-            if(DriftCheck == false)
-            {
-                DriftCheck = true;
-                CarDrift.Play();
-            }
-        }
-        else
-        {
-            DriftCheck = false;
+    // Méthode pour mettre à jour la rotation des roues
+    private void UpdateWheels()
+    {
+        // Mettre à jour la rotation de chaque roue
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheeTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+    }
 
-            Trail1.emitting = false;
-            Trail2.emitting = false;
-            Trail3.emitting = false;
-            Trail4.emitting = false;
-        }
+    // Méthode pour mettre à jour la rotation d'une seule roue
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        // Obtenir la position et la rotation de la roue dans le monde
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+
+        // Mettre à jour la rotation et la position de la roue dans l'espace du monde
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
     }
 }
